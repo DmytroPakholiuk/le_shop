e8305325fe6a498bc16ae49e9a01680598b8994e
dima <dmytro0pakhoilluk@gmail.com>
2023-02-21 14:25:45 +0200

    added dynamic form via pure js. attribute names and values are sent in separate arrays

--- backend/web/js/goods_form.js
+++ backend/web/js/goods_form.js
@@ -1,9 +1,42 @@
-
 function addAttribute(){
+    let attributesDiv = document.getElementById('attributeForm');
+    let rows = attributesDiv.children;
+
+    for (let row of rows) {
+       for (let col of row.children) {
+           let input = col.children[1];
+           if (input.value.length == 0){
+               alert('Please fill in existing attributes');
+               return
+           }
+       }
+    }
+
     let attributeInput = document.createElement('input');
     attributeInput.classList.add('form-control');
+    attributeInput.setAttribute('name', 'attributeName[]')
+    let attributeColDiv = document.createElement('div')
+    attributeColDiv.classList.add('col')
+    let attributeLabel = document.createElement('label');
+    attributeLabel.innerText = 'Attribute Name'
+
     let valueInput = document.createElement('input');
-    let attributesDiv = document.getElementById('attributeForm');
-    attributesDiv.appendChild(attributeInput);
-    attributesDiv.appendChild(valueInput);
+    valueInput.classList.add('form-control');
+    valueInput.setAttribute('name', 'attributeValue[]');
+    let valueColDiv = document.createElement('div');
+    valueColDiv.classList.add('col');
+    let valueLabel = document.createElement('label');
+    valueLabel.innerText = 'Attribute Value'
+
+    let rowDiv = document.createElement('div');
+    rowDiv.classList.add('row');
+
+    attributesDiv.appendChild(rowDiv);
+        rowDiv.appendChild(attributeColDiv);
+            attributeColDiv.appendChild(attributeLabel);
+            attributeColDiv.appendChild(attributeInput);
+        rowDiv.appendChild(valueColDiv);
+            valueColDiv.appendChild(valueLabel);
+            valueColDiv.appendChild(valueInput);
+
 }
