fb721ca26ad5b70963b9851258893a00cd9d6c77
dima <dmytro0pakhoilluk@gmail.com>
2023-09-29 16:15:42 +0300

    minor client id fix

--- docker/postgres/create_client.sql
+++ docker/postgres/create_client.sql
@@ -2,4 +2,4 @@ insert
 into oauth_clients
     (id, user_id, name, redirect, personal_access_client, password_client, revoked)
 values
-    (5, 1, 'pkce', 'http://view.le.shop:23000/auth', true, false, false);
+    (4, 1, 'pkce', 'http://view.le.shop:23000/auth', true, false, false);
